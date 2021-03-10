c5eb4605d61167529bd42275cd3483ddc0795a01
eilinwis <TTe4kiH98@gmail.com>
2021-03-10 13:38:57 +0300

    Miles created

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 331;
+        int stone = 20;
+
+        int miles = price / stone;
+        System.out.println(miles);
+
+    }
+}
